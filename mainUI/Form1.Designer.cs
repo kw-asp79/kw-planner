653f82e88de62cc5a8b5df89992c8f74928cb911
IyLias <iliaskyrin9707@gmail.com>
2023-04-28 22:25:41 +0900

    버튼 클릭 시 달력 넘어가는 것 구현

--- mainUI/Form1.Designer.cs
+++ mainUI/Form1.Designer.cs
@@ -29,9 +29,10 @@
         private void InitializeComponent()
         {
             this.calendarContainer = new System.Windows.Forms.Panel();
-            this.label8 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.dayContainer = new System.Windows.Forms.FlowLayoutPanel();
+            this.ymLbl = new System.Windows.Forms.Label();
+            this.prevBtn = new System.Windows.Forms.Button();
+            this.nextBtn = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -40,15 +41,14 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.settingBtn = new System.Windows.Forms.Button();
             this.loginBtn = new System.Windows.Forms.Button();
             this.todoBtn = new System.Windows.Forms.Button();
             this.fndBtn = new System.Windows.Forms.Button();
             this.lbyBtn = new System.Windows.Forms.Button();
             this.klasBtn = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.dayContainer = new System.Windows.Forms.FlowLayoutPanel();
             this.calendarContainer.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -56,9 +56,9 @@
             // calendarContainer
             // 
             this.calendarContainer.Controls.Add(this.dayContainer);
-            this.calendarContainer.Controls.Add(this.label8);
-            this.calendarContainer.Controls.Add(this.button2);
-            this.calendarContainer.Controls.Add(this.button1);
+            this.calendarContainer.Controls.Add(this.ymLbl);
+            this.calendarContainer.Controls.Add(this.prevBtn);
+            this.calendarContainer.Controls.Add(this.nextBtn);
             this.calendarContainer.Controls.Add(this.label7);
             this.calendarContainer.Controls.Add(this.label6);
             this.calendarContainer.Controls.Add(this.label5);
@@ -71,32 +71,42 @@
             this.calendarContainer.Size = new System.Drawing.Size(749, 725);
             this.calendarContainer.TabIndex = 0;
             // 
-            // label8
+            // dayContainer
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(354, 21);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(39, 12);
-            this.label8.TabIndex = 1;
-            this.label8.Text = "2023.4";
+            this.dayContainer.Location = new System.Drawing.Point(3, 88);
+            this.dayContainer.Name = "dayContainer";
+            this.dayContainer.Size = new System.Drawing.Size(742, 637);
+            this.dayContainer.TabIndex = 9;
             // 
-            // button2
+            // ymLbl
             // 
-            this.button2.Location = new System.Drawing.Point(173, 18);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(34, 18);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "<";
-            this.button2.UseVisualStyleBackColor = true;
+            this.ymLbl.AutoSize = true;
+            this.ymLbl.Font = new System.Drawing.Font("Segoe Script", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ymLbl.Location = new System.Drawing.Point(327, 9);
+            this.ymLbl.Name = "ymLbl";
+            this.ymLbl.Size = new System.Drawing.Size(93, 34);
+            this.ymLbl.TabIndex = 1;
+            this.ymLbl.Text = "2023.4";
             // 
-            // button1
+            // prevBtn
             // 
-            this.button1.Location = new System.Drawing.Point(536, 18);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(34, 18);
-            this.button1.TabIndex = 7;
-            this.button1.Text = ">";
-            this.button1.UseVisualStyleBackColor = true;
+            this.prevBtn.Location = new System.Drawing.Point(173, 18);
+            this.prevBtn.Name = "prevBtn";
+            this.prevBtn.Size = new System.Drawing.Size(34, 18);
+            this.prevBtn.TabIndex = 8;
+            this.prevBtn.Text = "<";
+            this.prevBtn.UseVisualStyleBackColor = true;
+            this.prevBtn.Click += new System.EventHandler(this.prevBtn_Click);
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Location = new System.Drawing.Point(536, 18);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(34, 18);
+            this.nextBtn.TabIndex = 7;
+            this.nextBtn.Text = ">";
+            this.nextBtn.UseVisualStyleBackColor = true;
+            this.nextBtn.Click += new System.EventHandler(this.nextBtn_Click);
             // 
             // label7
             // 
@@ -193,6 +203,24 @@
             this.panel1.Size = new System.Drawing.Size(186, 730);
             this.panel1.TabIndex = 1;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(52, 427);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(84, 25);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Chat";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(52, 336);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 25);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Group";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // settingBtn
             // 
             this.settingBtn.Location = new System.Drawing.Point(104, 702);
@@ -247,31 +275,6 @@
             this.klasBtn.Text = "KLAS";
             this.klasBtn.UseVisualStyleBackColor = true;
             // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(52, 336);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(84, 25);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "Group";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(52, 427);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(84, 25);
-            this.button4.TabIndex = 7;
-            this.button4.Text = "Chat";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // dayContainer
-            // 
-            this.dayContainer.Location = new System.Drawing.Point(3, 88);
-            this.dayContainer.Name = "dayContainer";
-            this.dayContainer.Size = new System.Drawing.Size(742, 637);
-            this.dayContainer.TabIndex = 9;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
@@ -292,8 +295,8 @@
         #endregion
 
         private System.Windows.Forms.Panel calendarContainer;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button prevBtn;
+        private System.Windows.Forms.Button nextBtn;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
@@ -301,7 +304,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label ymLbl;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button settingBtn;
         private System.Windows.Forms.Button loginBtn;
--- mainUI/Form1.cs
+++ mainUI/Form1.cs
@@ -12,6 +12,9 @@ namespace SampleCalendar
 {
     public partial class Form1 : Form
     {
+        private int month;
+        private int year;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,26 +22,28 @@ namespace SampleCalendar
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            displayDays();
+            DateTime now = DateTime.Now;
+            month = now.Month;
+            year = now.Year;
+            ymLbl.Text = year.ToString() + " . " + month.ToString();
+
+            displayDays(month,year);
         }
 
 
 
-        private void displayDays()
+        private void displayDays(int month, int year)
         {
-            DateTime now = DateTime.Now;
-
             // Get 1st day of current month and current year
-            DateTime startOfMonth = new DateTime(now.Year, now.Month, 1);
+            DateTime startOfMonth = new DateTime(year, month, 1);
 
-            int days = DateTime.DaysInMonth(now.Year, now.Month);
+            int days = DateTime.DaysInMonth(year, month);
             int daysOfWeek = Convert.ToInt32(startOfMonth.DayOfWeek.ToString("d"))+1;
 
             for (int i = 1; i < daysOfWeek; i++)
             {
                 UserControlBlank ucBlank = new UserControlBlank();
                 dayContainer.Controls.Add(ucBlank);
-                
             }
 
             for (int i = 1; i <= days; i++)
@@ -46,11 +51,41 @@ namespace SampleCalendar
                UserControlDays ucDays = new UserControlDays();
                ucDays.days(i);
                dayContainer.Controls.Add(ucDays);
-                
             }
 
         }
 
+        // show next month of the calendar
+        private void nextBtn_Click(object sender, EventArgs e)
+        {
+            dayContainer.Controls.Clear();
 
+            // if month exceeds 12, i.e, next year
+            if(++month > 12)
+            {
+                month = 1;
+                year++;
+            }
+
+            ymLbl.Text = year.ToString() + " . " + month.ToString();
+            displayDays(month, year);
+        }
+
+
+        // show previous month of the calendar
+        private void prevBtn_Click(object sender, EventArgs e)
+        { 
+            dayContainer.Controls.Clear();
+
+            // if month less than 1, i.e, previous year
+            if(--month < 1)
+            {
+                month = 12;
+                year--;
+            }
+
+            ymLbl.Text = year.ToString() + " . " + month.ToString();
+            displayDays(month, year);
+        }
     }
 }
