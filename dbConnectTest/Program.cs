9f1a6f47305d531fc7218b4a463f4bab17aec9fc
seonghooni <lsh3717@naver.com>
2023-05-04 10:21:40 +0900

    DB 연결

---
+++ dbConnectTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace dbConnectTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection("Server=localhost;Database=schema;Uid=root;Pwd=deokwon11@");
+
+                connection.Open();
+
+                string Query = "INSERT INTO `schema`.`user` (`user_id`, `pwd`, `name`) VALUES ('12', '12', 'abc');";
+
+                MySqlCommand command = new MySqlCommand(Query, connection);
+
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                }
+
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+    }
+}
+
