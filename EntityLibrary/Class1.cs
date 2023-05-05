227acdee862a1a2edb5abe6b581fe36fd7bfc683
seonghooni <lsh3717@naver.com>
2023-05-05 13:42:18 +0900

    EntityLibrary 제작중

---
+++ EntityLibrary/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLibrary
+{
+    public class User
+    {
+        public string id;
+        public string pwd;
+        public string name;
+
+        public User()
+        {
+            this.id = "";
+            this.pwd = "";
+            this.name = "";
+        }
+
+        public User(string id, string pwd, string name)
+        {
+            this.id = id;
+            this.pwd = pwd;
+            this.name = name;
+        }
+        
+
+    }
+}
