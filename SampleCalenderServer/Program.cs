04a22de0bb9bf08cbe1fdfa22dcc8baf9a3cd424
gusgkr527 <gusgkr527@naver.com>
2023-05-23 13:03:44 +0900

    DB 저장

--- SampleCalenderServer/Program.cs
+++ SampleCalenderServer/Program.cs
@@ -6,115 +6,364 @@ using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Net;
 using MySql.Data.MySqlClient;
+using EntityLibrary;
+using PacketLibrary;
+using System.Management.Instrumentation;
+using MySqlX.XDevAPI;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+using System.Data.Odbc;
+using System.Collections;
+using System.Runtime.Remoting.Messaging;
+using System.Data.SqlClient;
 
 namespace Server
 {
-    internal class Program
+
+    public class DBProcess
     {
-        static void Main(string[] args)
-        {            
+        public static string connectionString;
+        public static MySqlConnection connection;
 
-            // 데이터베이스 연결 
-            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ProjectDB"].ConnectionString;
+        public static User SelectUser(string user_id, string user_pwd)
+        {
+            MySqlCommand command = connection.CreateCommand();
 
-            MySqlConnection connection = new MySqlConnection(connectionString);
+            command.CommandText = "SELECT * FROM user WHERE user_id = @user_id and pwd = @pwd;";
+            command.Parameters.AddWithValue("@user_id", user_id);
+            command.Parameters.AddWithValue("@pwd", user_pwd);
 
+            MySqlDataReader reader = command.ExecuteReader();
+
+            User user = new User();
+
+            // 데이터가 있다면
+            if (reader.HasRows)
+            {
+                reader.Read();
+
+                user.id = reader.GetString("user_id");
+                user.pwd = reader.GetString("pwd");
+                user.name = reader.GetString("name");
+            }
+
+            reader.Close();
+
+            return user;
+        }
+
+        public static List<User> SelectFriends(User user)
+        {
+            MySqlCommand command = connection.CreateCommand();
+
+            command.CommandText = "SELECT user.* FROM user JOIN friendship WHERE friendship.user_id = @myUserId AND friendship.friend_id = user.user_id;";
+            command.Parameters.AddWithValue("@myUserId", user.id);
+
+            MySqlDataReader reader = command.ExecuteReader();
+
+            List<User> friends = new List<User>();
+
+            while (reader.Read())
+            {
+                User friend = new User();
+
+                friend.id = reader.GetString("user_id");
+                friend.name = reader.GetString("name");
+
+                friends.Add(friend);
+            }
+
+
+            reader.Close();
+
+            return friends;
+        }
+
+        public static List<Schedule> SelectSchedules(User user)
+        {
+            MySqlCommand command = connection.CreateCommand();
+
+            command.CommandText = "SELECT schedule.* FROM schedule JOIN user_schedule WHERE user_schedule.user_id = @myUserId AND user_schedule.schedule_id = schedule.schedule_id;";
+            command.Parameters.AddWithValue("@myUserId", user.id);
+
+            MySqlDataReader reader = command.ExecuteReader();
+
+            List<Schedule> schedules = new List<Schedule>();
+
+            while (reader.Read())
+            {
+                Schedule schedule = new Schedule();
+
+                schedule.category = reader.GetString("category");
+                schedule.title = reader.GetString("title");
+                schedule.content = reader.GetString("content");
+                schedule.startTime = reader.GetDateTime("start_time");
+                schedule.endTime = reader.GetDateTime("end_time");
+
+                schedules.Add(schedule);
+            }
+
+            reader.Close();
+
+            return schedules;
+        }
+        public static Dictionary<string, List<User>> SelectGroupsWithFriends(User user)
+        {
+            // 나한테만 보여지는 Group이라고 가정
+            MySqlCommand command = connection.CreateCommand();
+
+            command.CommandText = "SELECT group.name FROM schema.group WHERE group.user_id = @myUserId;";
+            command.Parameters.AddWithValue("@myUserID", user.id);
+
+            MySqlDataReader reader = command.ExecuteReader();
+
+            Dictionary<string, List<User>> groups = new Dictionary<string, List<User>>();
+
+            while (reader.Read())
+            {
+                string groupName = reader.GetString("name");
+                groups.Add(groupName, new List<User>());
+            }
+
+            reader.Close();
+
+            foreach (string groupName in groups.Keys)
+            {
+                command = connection.CreateCommand();
+                command.CommandText = "SELECT user.* FROM user join user_group join schema.group where group.name = @groupName" +
+                    " and schema.group.group_id = user_group.group_id and user_group.user_id = user.user_id;";
+                command.Parameters.AddWithValue("@groupName", groupName);
+
+                reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+                    User friend = new User();
+
+                    friend.id = reader.GetString("user_id");
+                    friend.name = reader.GetString("name");
+
+                    groups[groupName].Add(friend);
+                }
+
+                reader.Close();
+            }
+
+            foreach (string groupName in groups.Keys)
+            {
+                Console.Write("my Group: {0} : ", groupName);
+                groups[groupName].ForEach(element => Console.Write(" {0}", element.name));
+                Console.WriteLine();
+            }
+
+            return groups;
+        }
+
+
+        public static void ConnectDB()
+        {
+
+            connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ProjectDB"].ConnectionString;
+            connection = new MySqlConnection(connectionString);
 
             try
             {
-                // 데이터베이스 연결
                 connection.Open();
-
-                MySqlCommand command = connection.CreateCommand();
-
-                //초기값 설정 
-                string name = "testGroup";
-                string pwd = "password123";
-                int User_id = 100;
-                int schedule_id = 0;
-                string category = "카테고리";
-                string title = "제목";
-                string content = "내용";
-                DateTime datetime = DateTime.Now;
-                int group_id = 10090;
-                int friendship_id = 0;
-                int friend_id = 0;
-                int user_group_id = 0;
-                int user_schedule_id = 0;
-                
-                //command.CommandText는 객체의 속성, 퀴리문 저장하는데 사용 
-                command.CommandText = "INSERT INTO asp.group (name, group_id) VALUES (@name1, @group_id1);";
-                command.Parameters.AddWithValue("@name1", name);
-                command.Parameters.AddWithValue("@group_id1", group_id);
-                //쿼리를 실행하는 메서드
-                command.ExecuteNonQuery();
-
-                command.CommandText = "INSERT INTO asp.Schedule(schedule_id, category, title, content, datetime) VALUES (@schedule_id1, @category, @title, @content, @datetime);";
-                command.Parameters.AddWithValue("@schedule_id1", schedule_id); // 올리는 과정@ 
-                command.Parameters.AddWithValue("@category", category);
-                command.Parameters.AddWithValue("@title", title);
-                command.Parameters.AddWithValue("@content", content);
-                command.Parameters.AddWithValue("@datetime", datetime);
-                command.ExecuteNonQuery();// 업데이트하는 과정@ 
-
-                command.CommandText = "INSERT INTO asp.User(user_id, pwd, name) VALUES (@user_id1, @pwd, @name2);";
-                command.Parameters.AddWithValue("@user_id1", User_id);
-                command.Parameters.AddWithValue("@pwd", pwd);
-                command.Parameters.AddWithValue("@name2", name);
-                command.ExecuteNonQuery();
-
-                command.CommandText = "INSERT INTO asp.Friendship(friendship_id, user_id, friend_id) VALUES (@friendship_id, @user_id2, @friend_id);";
-                command.Parameters.AddWithValue("@friendship_id", friendship_id);
-                command.Parameters.AddWithValue("@user_id2", User_id);
-                command.Parameters.AddWithValue("@friend_id", friend_id);
-                command.ExecuteNonQuery();
-
-                command.CommandText = "INSERT INTO asp.User_group(user_group_id, group_id, user_id) VALUES (@user_group_id, @group_id2, @user_id3);";
-                command.Parameters.AddWithValue("@user_group_id", user_group_id);
-                command.Parameters.AddWithValue("@group_id2", group_id);
-                command.Parameters.AddWithValue("@user_id3", User_id);
-                command.ExecuteNonQuery();
-
-                command.CommandText = "INSERT INTO asp.User_schedule(user_schedule_id, user_id, schedule_id) VALUES (@user_schedule_id, @user_id4, @schedule_id2);";
-                command.Parameters.AddWithValue("@user_schedule_id", user_schedule_id);
-                command.Parameters.AddWithValue("@user_id4", User_id);
-                command.Parameters.AddWithValue("@schedule_id2", schedule_id);
-                command.ExecuteNonQuery();
-
-                Console.WriteLine("데이터가 성공적으로 삽입되었습니다.");
             }
             catch (Exception ex)
             {
-                // 예외 처리
-                Console.WriteLine("오류 발생: " + ex.Message);
+                Console.WriteLine(ex.Message.ToString());
             }
-            finally
+        }
+    }
+
+    public class Program
+    {
+
+        public static Packet LoginProcess(User user)
+        {
+            Packet sendPacket = new Packet();
+
+            User result = DBProcess.SelectUser(user.id, user.pwd);
+
+            // select 결과가 비어있으면 
+            if (result.isEmpty())
             {
-                // 연결 해제
-                connection.Close();
+                sendPacket.data = null;
+                Console.WriteLine(".. id:{0}, pwd:{1}.. fail", user.id, user.pwd);
+            }
+            else
+            {
+                sendPacket.data = result;
+                Console.WriteLine(".. id:{0}, pwd:{1}.. success", user.id, user.pwd);
+            }
+            sendPacket.action = ActionType.Response;
+
+            return sendPacket;
+        }
+
+        public static Packet ReadAllDataProcess(User user)
+        {
+            Packet sendPacket = new Packet();
+
+            Dictionary<String, Object> fullData = new Dictionary<String, Object>();
+
+            List<User> friends = DBProcess.SelectFriends(user);
+            List<Schedule> schedules = DBProcess.SelectSchedules(user);
+            Dictionary<String, List<User>> groups = DBProcess.SelectGroupsWithFriends(user);
+
+            fullData.Add("friends", friends);
+            fullData.Add("schedules", schedules);
+            fullData.Add("groups", groups);
+
+            sendPacket.action = ActionType.Response;
+            sendPacket.data = fullData;
+
+            return sendPacket;
+        }
+
+
+        async static void AsyncProcess(Object o)
+        {
+            TcpClient client = (TcpClient)o;
+            NetworkStream netstrm = client.GetStream();
+
+            // TcpClient의 Socket 객체를 가져옴
+            Socket socket = client.Client;
+            string remoteAddress = socket.RemoteEndPoint.ToString();
+
+            while (client.Connected)
+            {
+                PacketInfo packetInfo = new PacketInfo();
+                Packet receivedPacket;
+                Packet sendPacket = new Packet();
+
+                byte[] size = new byte[4];
+
+                int recv = await netstrm.ReadAsync(size, 0, 4).ConfigureAwait(false);
+                packetInfo.size = BitConverter.ToInt32(size, 0);
+
+                byte[] data = new byte[packetInfo.size];
+
+                recv = await netstrm.ReadAsync(data, 0, packetInfo.size);
+
+                receivedPacket = Packet.Desserialize(data, packetInfo);
+
+
+                switch (receivedPacket.action)
+                {
+                    case ActionType.login:
+                        Console.Write("[{0}] login request", remoteAddress);
+
+                        User user = (User)receivedPacket.data;
+
+                        sendPacket = LoginProcess(user);
+                        break;
+                    case ActionType.readAllData:
+                        Console.WriteLine("[{0}] readAllData request", remoteAddress);
+
+                        User user1 = (User)receivedPacket.data;
+
+                        sendPacket = ReadAllDataProcess(user1);
+                        break;
+                    case ActionType.deleteUser:
+                        break;
+                    case ActionType.editUser:
+                        break;
+                    default:
+                        break;
+                }
+
+                // 응답을 전송함
+
+                data = Packet.Serialize(sendPacket, packetInfo);
+                size = BitConverter.GetBytes(packetInfo.size); ;
+
+                // packet의 size를 먼저 전송
+                await netstrm.WriteAsync(size, 0, 4);
+                // 그 다음 packet을 전송
+                await netstrm.WriteAsync(data, 0, packetInfo.size);
+                netstrm.Flush();
             }
 
+            netstrm.Close();
+            client.Close();
+        }
 
+        async static Task AsyncServer()
+        {
 
+            TcpListener server = new TcpListener(9050);
 
+            server.Start();
 
+            while (true)
+            {
+                TcpClient client = await server.AcceptTcpClientAsync().ConfigureAwait(false);
+
+                Task.Run(() => AsyncProcess(client));
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            DBProcess.ConnectDB();
+
+            AsyncServer().Wait();
+
+            /*
+
+            
+            *//*
+            TcpListener server = new TcpListener(9050);
+
+            server.Start();
+
+            while(true)
+            {
+                server.BeginAcceptTcpClient(new AsyncCallback(AcceptCallback), server);
+            }
+            */
+
+            /*
             // TCP 통신
-            int receive;
-            byte[] data = new byte[1024];
+            TcpListener server = new TcpListener(9050);
 
-            TcpListener newSock = new TcpListener(9050);
-
-            newSock.Start();
+            server.Start();
             Console.WriteLine("Waiting for a client....");
 
-            TcpClient client = newSock.AcceptTcpClient();
-            // NetworkStream ns = client.GetStream();
+            TcpClient client = server.AcceptTcpClient();
+            NetworkStream netstrm = client.GetStream();
 
             Console.WriteLine("Client Connected.. : {0}", client.Client.RemoteEndPoint);
 
-            // ns.Close();
+            // 반복문 내에서 실행될 예정임
+            // while (true) {  }
+            Packet receivedPacket = Packet.ReceivePacket(netstrm);
+
+            switch(receivedPacket.action)
+            {
+                case ActionType.saveUser:
+                    User user = (User)receivedPacket.data;
+                    // saveUser(User)함수 실행
+                    Console.WriteLine("action : {0}", receivedPacket.action.ToString());
+                    Console.WriteLine("id : {0}, pwd : {1}, name : {2}", user.id, user.pwd, user.name);
+                    break;
+                case ActionType.deleteUser:
+                    break;
+                case ActionType.editUser:
+                    break;
+                default:
+                    break;
+            }
+            
+            netstrm.Close();
             client.Close();
-            newSock.Stop();
+            server.Stop();
+
+            */
         }
     }
 }
